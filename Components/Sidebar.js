6ab1b1ea589508e1feb6becfbde42eb663fb54bb
Alex <alexbachurin91@gmail.com>
2022-03-28 01:10:30 +0300

    initial setup and structure

---
+++ Components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Avatar } from '@mui/icons-material/AccessAlarm'
+const Sidebar = () => {
+    return (
+        <div className='flex-[0.35] flex-col bg-testColor'>
+            {/* Sidebar Header */}
+
+            {/* Search Box */}
+            {/* Chat Section */}
+        </div>
+    )
+}
+
+export default Sidebar
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      mainBg: '#dadbd3',
+      appBg: '#ebebeb',
+      testColor: '#008080'
+    },
+    boxShadow: {
+      mainShadow: '-1px 4px 20px -6px rgba(0,0,0,0.2)'
+    }
   },
   plugins: [],
 }
